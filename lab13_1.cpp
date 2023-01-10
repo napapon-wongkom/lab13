5b6a38d24a66a00afb95e46ef44715ed3d88c469
napapon-wongkom <dot.wongkom@gmail.com>
2023-01-10 15:13:19 +0700

    lab13_1.cpp

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,20 @@
 #include<iostream>
 using namespace std;
-
+int fibonacci(int);
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
+}
+int fibonacci(int x){
+    int ans;
+    if(x==0){
+        ans = 0;
+    }else if(x==1){
+        ans =1;
+    }else if(x>1){
+        ans = fibonacci(x-1) + fibonacci(x-2);
+    }
+    return ans;
 }
