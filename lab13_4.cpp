cf22a3197fa13d009a4fc12a8bc7fa2972052397
napapon-wongkom <dot.wongkom@gmail.com>
2023-01-10 16:04:15 +0700

    lab13_4.cpp

--- lab13_4.cpp
+++ lab13_4.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 
 using namespace std;
-
+template <typename type>
+void mySwap(type &x,type &y);
 int main(){
 	int x, y;
 	string a, b;
@@ -28,5 +29,12 @@ int main(){
 	cout << "p = " << p << ", q = " << q << "\n";
 
 	return 0;
+}
+template <typename type>
+void mySwap(type &x,type &y){
+	type newx = y;
+	type newy =x;
+	x = newx;
+	y = newy;
 }
 
